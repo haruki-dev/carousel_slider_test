bb13b68e18c41b07b3d75f5aea9597ec665d2261
haruki-dev <euphotoe@gmail.com>
2024-09-26 02:00:38 +0900

    '202409260200'

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-// import 'package:carousel_slider/carousel_state.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
+
+class TestPage{
+  final String name;
+
+  final String description;
+
+  TestPage(this.name, this.description);
+}
+
+final models = [
+  TestPage('test1', '1ページ目'),
+  TestPage('test2', '2ページ目'),
+  TestPage('test3', '3ページ目'),
+  TestPage('test4', '4ページ目'),
+  TestPage('test5', '5ページ目'),
+];
+
+class TestTest extends StatelessWidget{
+  const TestTest({
+    super.key,
+    required this.model,
+  });
+
+  final TestPage model;
+
+  @override
+  Widget build(BuildContext context){
+  return Container(
+      decoration: BoxDecoration(
+        // 色
+        color: Colors.blueGrey,
+        // 角丸
+        borderRadius: BorderRadius.circular(20),
+        // 影
+        boxShadow: [
+          BoxShadow(
+            // 影の設定
+            color: Colors.black.withOpacity(0.2), //色
+            spreadRadius: 2, // 広がりの大きさ
+            blurRadius: 2, // ぼかしの強さ
+            offset: const Offset(0, 2), // 方向
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget modelToWidget(TestPage model) {
+
+  // ページの部分
+  return Container(
+    // カードの周りに 10 ずつスペースを空ける
+    padding: const EdgeInsets.fromLTRB(10,50,10,50),
+    // 中身は カード
+    child: TestTest(model: model),
+  );
+  // カードを使う
+}
+
+
 
 class Home extends StatefulWidget{
   const Home({super.key});
@@ -9,93 +69,38 @@ class Home extends StatefulWidget{
 }
 
 class _HomeState extends State<Home> {
-  
-  void initState(){
-    super.initState();
-      CarouselControllerImpl _impl = CarouselControllerImpl();
-      CarouselSliderState _state = CarouselSliderState(_impl);
-      final _controller = PageController();
-      int _currentPage = _controller.page!.floor();
-  }
-  
+
   double x = 0;
   double y = 0;
   double z = 0;
 
+  int _currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
+    final carousel =PageView.builder(
+      controller: PageController(
+        viewportFraction: 0.8,
+      ),
+      itemCount: models.length,
+      itemBuilder: (c, i) => modelToWidget(models[i]),
+      onPageChanged: (int page){
+        setState(() {
+          _currentPage = page;
+        });
+        print(_currentPage);
+      },
+    );
     return  Scaffold(
       body:Center(
-        child: CarouselSlider(
-          options:CarouselOptions(
-            height: 600.0,
-            viewportFraction: 0.75,
-            enlargeCenterPage: true,
-            enlargeFactor: 0.2,
-            ),
-          items: [1,2,3,4,5].map((i) {
-            if ( _currentPage > i-1){
-              return Builder(
-                builder: (BuildContext context){
-                  return Container(
-                  transform: Matrix4(
-                    1,0,0,0,
-                    0,1,0,0,
-                    0,0,1,0,
-                    0,0,0,1.5,
-                  )..rotateX(x)..rotateY(y)..rotateZ(z),
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.blueGrey
-                  ),
-                  child: Center(child: Text('text $i', style: TextStyle(fontSize: 12.0),))
-                  );
-                },
-              );
-            } 
-            else if (i+1 > i ){
-              return Builder(
-                builder: (BuildContext context){
-                  return Container(
-                  transform: Matrix4(
-                    1,0,0,0,
-                    0,1,0,0,
-                    0,0,1,0,
-                    0,0,0,2,
-                  )..rotateX(x)..rotateY(y)..rotateZ(z),
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.blueGrey
-                  ),
-                  child: Center(child: Text('text $i', style: TextStyle(fontSize: 12.0),))
-                  );
-                },
-              );
-            } 
-            else {
-            return Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.blueGrey
-                  ),
-                  child: Center(child: Text('text $i', style: TextStyle(fontSize: 12.0),))
-                );
-              },
-            );
-            }
-          },
-          ).toList(),
+        child: Container(
+          height: 730,
+          child: carousel,
         ),
-      )
-
+      ),
+    );
+  }
+}
       // 別パッケージを試行した名残り
 
       // import 'package:another_transformer_page_view/another_transformer_page_view.dart';
@@ -131,6 +136,3 @@ class _HomeState extends State<Home> {
           //   ),
           // )
       // )
-    );
-  }
-}
